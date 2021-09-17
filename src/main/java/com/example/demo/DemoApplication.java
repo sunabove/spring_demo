3da59e0fd2d9d6e12d29ea800071838de61f1df3
sunabove <sunabove@users.noreply.github.com>
2021-09-17 16:08:28 +0900

    **

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.*;
+
+@SpringBootApplication
+@RestController
+@ComponentScan
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MyController {
+	@GetMapping("/myHello.abc")
+	public String hello(Model model, @RequestParam(value = "name", defaultValue = "World") String name) {
+		var tasks = java.util.Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+		model.addAttribute("myData", "Good Morning!");
+		model.addAttribute("name", name);
+		model.addAttribute("tasks", tasks);
+		return "myHello.html";
+	}
+}
