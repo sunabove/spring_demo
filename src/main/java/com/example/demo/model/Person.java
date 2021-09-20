cbb7c01adc62d148cfd91511780b73f9e285dfdb
sunabove <sunabove@nate.com>
2021-09-20 22:24:34 +0900

    **

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import java.util.Date;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "name", length = 50, nullable = false, unique = false)
+	private String name;
+	@Transient
+	private Integer age;
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
+}
---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "STUDENT_NAME", length = 50, nullable = false, unique = false)
+	private String name;
+	@Transient
+	private Integer age;
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+}
