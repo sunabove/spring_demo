316fb0d607d5116bbbfd01aafdb27c2248eb7f8a
sunabove <sunabove@nate.com>
2021-10-01 13:44:47 +0900

    **

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,21 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.context.annotation.*;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan
-
-//@EnableJpaRepositories( "com.example.demo.model" )
-//@EntityScan( basePackages = {"com.example.demo.model"} )
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	} 
+	}
 
 }
